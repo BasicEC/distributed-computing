bb9169c8aa21fe0a7d8777a7ecf9f281789e369a
edvanchi <edvanchi108@gmail.com>
2019-03-08 19:35:19 +0300

    distributed system initialization created

--- task1/src/main.c
+++ task1/src/main.c
@@ -9,11 +9,60 @@
 
 int PROCESS_COUNT;
 
-void do_smth(){
+typedef void(*process_task)();
+
+
+typedef struct {
+    int read;
+    int write;
+} connection;
+
+
+typedef struct {
+  int id;
+  process_task task;
+  int connection_count;
+  connection* connections;
+} proc_info;
+
+
+typedef struct {
+  int process_count;
+  proc_info* processes;
+}System;
+
+
+int send_greeting(){
 
 }
 
-int create_process(){
+int send_parting(){
+
+}
+
+void do_smth()
+{
+    send_greeting();
+    send_parting();
+}
+
+void establish_connection(){
+
+}
+
+void establish_all_connections(System* sys){
+    int i,j;
+    for (i = 0; i < sys->process_count; i++){
+        for (j = 0; j < sys->process_count; j++){
+            establish_connection();
+        }
+    }
+
+}
+
+
+int create_process(process_task task)
+{
    pid_t id = fork();
    if (id < 0) {
        perror("unable to create process");
@@ -21,17 +70,31 @@ int create_process(){
        _exit(-1);
    }
    if (id == 0){
-       do_smth();
+       task();
        _exit(0);
    }
     return id;
 }
 
-void run(){
+void initialize_child(proc_info* child){
+    child->connections = malloc(sizeof(connection)* (PROCESS_COUNT - 1));
+}
+
+System* initialize_System(){
+    System* sys = (System*)malloc(sizeof(System));
+    sys->process_count = PROCESS_COUNT;
+    proc_info* children = (proc_info*)malloc(sizeof(proc_info));
+    sys->processes = children;
+    int i;
+    for (i = 0; i < sys->process_count; i++)
+        initialize_child(sys->processes + i);
+}
+
+void run(process_task task){
     int i;
     int pid_arr[PROCESS_COUNT];
     for(i = 0; i < PROCESS_COUNT; i++){
-        pid_arr[i] = create_process();
+        pid_arr[i] = create_process(task);
     }
 }
 
@@ -45,8 +108,9 @@ void parse_arguments(char** args)
 int main(int argc, char** argv)
 {
     parse_arguments(argv);
-    printf("%d",PROCESS_COUNT);
-    run();
+    System* sys = initialize_System();
+    establish_all_connections(sys);
+    run(do_smth);
     return 0;
 }
 
