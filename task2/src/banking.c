720e51430ade7269f1ad15f24188799cf603f88d
Vitaliy Zakusilo <v.zakusilo@studiomobile.ru>
2019-04-02 14:52:59 +0300

    init banking.c

---
+++ task2/src/banking.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "banking.h"
+#include "self.h"
+
+void transfer(void * proc_info, local_id src, local_id dst, balance_t amount) {
+  
+  TransferOrder to;
+  to.s_src = src;
+  to.s_dst = dst;
+  to.s_amount = amount;
+  
+  Message m;
+  Message m_received;
+  m.s_header.s_magic = MESSAGE_MAGIC;
+  m.s_header.s_type = TRANSFER;
+  m.s_header.s_local_time = get_physical_time();
+  m.s_header.s_payload_len = sizeof(TransferOrder);
+
+  memcpy(m.s_payload, &to, sizeof(TransferOrder));
+  send(proc_info, src, &m);
+  receive(proc_info, dst, &m_received);
+}
