dc6e9a91a86882ffbce12570261ef348194911da
BasicEC <zak.vitaly@ya.ru>
2019-04-03 17:04:16 +0300

    child main_work() template

--- task2/src/child.c
+++ task2/src/child.c
@@ -1,6 +1,6 @@
 #include "child.h"
 
-static int send_to_all_and_wait_all(proc_info_t *proc, MessageType type)
+static void send_to_all_and_wait_all(proc_info_t *proc, MessageType type)
 {
     char payload[COMMON_PAYLOAD_LEN];
     int len = sprintf(payload, log_started_fmt, get_physical_time(), id, getpid(), getppid(), proc->balance);
@@ -8,16 +8,47 @@ static int send_to_all_and_wait_all(proc_info_t *proc, MessageType type)
     Message msg = create_message(payload, len, STARTED);
 
     send_multicast(proc, msg);
-    // usleep(1);
     Message *msgs[proc->connection_count];
     for (int i = 1; i < proc->connection_count - 1; i++)
     {
         msgs[i] = (Message *)malloc(sizeof(Message));
         //не receive_any потому что какой-то процесс может успеть послать 2 сообщения?
-        receive(proc, proc->id, msgs[i]); 
+        receive(proc, proc->id, msgs[i]);
     }
+}
 
-    return 0;
+static void main_work(proc_info_t *proc)
+{
+    BalanceHistory history;
+    BalanceState state;
+    Message msg;
+    timestamp_t last_time = 0;
+    int done_count = 0;
+    int balance = proc->balance;
+    history.s_id = proc->id;
+    state.s_balance = balance;
+    state.s_time = 0;
+    state.s_balance_pending_in = 0;
+    history.s_history[0] = state;
+    int proc->connection_count;
+    while (1)
+    {
+        receive_any(proc, &msg);
+        switch (msg.s_header.s_type)
+        {
+        case TRANSFER:
+            on_transfer();
+            break;
+        case STOP:
+            on_stop();
+            break;
+        case DONE:
+            on_done();
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 void child_work(System_t *sys, local_id id)
