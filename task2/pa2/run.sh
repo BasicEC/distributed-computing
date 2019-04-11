76d66bee8f3292a616e12d48c750605b2efa96cd
edvanchi <artur.gogiyan@gmail.com>
2019-04-11 18:57:30 +0300

    sh script fix

--- task2/pa2/run.sh
+++ task2/pa2/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-LD_PRELOAD="/mnt/d/Univer/6sem/PAs/distributed-computing/task2/pa2/libruntime.so" ./main -p 2 10 20
+export LD_LIBRARY_PATH="$PWD"
+
+LD_PRELOAD="$PWD/libruntime.so" ./main $@
